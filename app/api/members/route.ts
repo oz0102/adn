e02b9fa8ee129e98504fd296786dc3e624fcfc6e
oz0102 <flippinghq@gmail.com>
2025-05-26 14:10:14 +0100

    fixed cluster page

--- app/api/members/route.ts
+++ app/api/members/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/auth";
-import dbConnect from "@/lib/dbConnect";
+// import dbConnect from "@/lib/dbConnect";
+import { connectToDB } from "@/lib/mongodb";
 import Member from "@/models/member";
 
 export async function GET(request: NextRequest) {
